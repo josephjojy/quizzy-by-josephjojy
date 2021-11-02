968acf0944d618af1a3941c46fe5881895cbe3b1
Joseph Jojy <joseph.jojy61@gmail.com>
2021-11-02 15:21:43 +0530

    Added to lower case for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   validates :email, :first_name, :last_name, presence: true
   validates :first_name, :last_name, length: { maximum: 50 }
   validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  before_save :to_lowercase
+
+  private
+
+    def to_lowercase
+      email.downcase!
+    end
 end
