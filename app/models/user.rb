4fb1b07ae996211441f9810dfcccbdfda11bbb5d
Joseph Jojy <joseph.jojy61@gmail.com>
2021-11-02 15:13:48 +0530

    Added regex for email in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
   validates :email, :first_name, :last_name, presence: true
   validates :first_name, :last_name, length: { maximum: 50 }
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
