8c61c1851cdda0ebc81c6fbccf184a8f7b8fbc52
Joseph Jojy <joseph.jojy61@gmail.com>
2021-11-03 10:41:05 +0530

    added has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   enum role: { standard: 0, administrator: 1 }
 
+  has_secure_password
+
   validates :email, :first_name, :last_name, presence: true
   validates :first_name, :last_name, length: { maximum: 50 }
   validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
