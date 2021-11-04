398edfe6b6da685ecef3db0ffed5df2a278598bd
Joseph Jojy <joseph.jojy61@gmail.com>
2021-11-05 04:31:39 +0530

    added Logout feature

---
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <p className=""> DASHBOARD</p>
+    </div>
+  );
+};
+
+export default Dashboard;
--- app/javascript/src/components/NavBar/index.jsx
+++ app/javascript/src/components/NavBar/index.jsx
@@ -1,11 +1,44 @@
 import React from "react";
 
+import { Typography, Button } from "@bigbinary/neetoui/v2";
 import { Header } from "@bigbinary/neetoui/v2/layouts";
+import { resetAuthTokens } from "src/apis/axios";
 
-const NavBar = () => {
+import authApi from "apis/auth";
+import { getFromLocalStorage, setToLocalStorage } from "helpers/storage";
+
+const NavBar = ({ isLoggedIn }) => {
+  const userName = getFromLocalStorage("authUserName");
+  const handleLogout = async () => {
+    try {
+      await authApi.logout();
+      setToLocalStorage({
+        authToken: null,
+        email: null,
+        userId: null,
+        userName: null,
+      });
+      resetAuthTokens();
+      window.location.href = "/";
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
   return (
-    <div className="border-b-2">
-      <Header title="Quizzy" className="ml-8" />
+    <div className="border-b-2 mx-5">
+      <Header
+        title="Quizzy"
+        actionBlock={
+          isLoggedIn && (
+            <>
+              <p>Reports</p>
+              <Typography className="px-4">{userName}</Typography>
+              <Button label="Logout" onClick={() => handleLogout()} />
+            </>
+          )
+        }
+      />
     </div>
   );
 };
