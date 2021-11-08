41e1e36adf634f4183456a8caa285ec6c2cd84a2
Joseph Jojy <joseph.jojy61@gmail.com>
2021-11-08 20:34:33 +0530

    Use ActiveRecord Relation to create the quiz - solved #31

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -10,7 +10,7 @@ class QuizzesController < ApplicationController
   end
 
   def create
-    quiz = Quiz.new(quiz_params.merge(user_id: @current_user.id))
+    quiz = @current_user.quizzes.new(quiz_params)
     authorize quiz
     if quiz.save
       render status: :ok, json: { notice: "Successfully created Quiz" }
