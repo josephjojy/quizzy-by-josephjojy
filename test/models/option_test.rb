8c63dd4551589a590364616729bffe0b82f21044
Joseph Jojy <joseph.jojy61@gmail.com>
2021-11-11 23:23:24 +0530

    added Tests

--- test/models/option_test.rb
+++ test/models/option_test.rb
@@ -3,7 +3,35 @@
 require "test_helper"
 
 class OptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(
+      first_name: "Sam",
+      last_name: "Smith",
+      email: "sam@example.com",
+      password: "welcome",
+      password_confirmation: "welcome"
+    )
+    @quiz = Quiz.create(
+      name: "New Quiz",
+      user_id: @user.id
+    )
+    @question = Question.create(
+      content: "Some Question",
+      quiz_id: @quiz.id
+    )
+    @option = Option.new(
+      content: "Answer A",
+      question_id: @question.id
+    )
+  end
+
+  def test_option_valid
+    assert @option.valid?
+  end
+
+  def test_option_should_have_content
+    @option.content = ""
+    assert @option.invalid?
+    assert_includes @option.errors.full_messages, "Content can't be blank"
+  end
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -3,7 +3,37 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(
+      first_name: "Sam",
+      last_name: "Smith",
+      email: "sam@example.com",
+      password: "welcome",
+      password_confirmation: "welcome"
+    )
+    @quiz = Quiz.create(
+      name: "New Quiz",
+      user_id: @user.id
+    )
+    @question = Question.new(
+      content: "Some Question",
+      quiz_id: @quiz.id
+    )
+  end
+
+  def test_question_valid
+    assert @question.valid?
+  end
+
+  def test_question_should_not_be_valid_without_quiz
+    @question.quiz_id = nil
+    assert @question.invalid?
+    assert_includes @question.errors.full_messages, "Quiz must exist"
+  end
+
+  def test_question_should_have_content
+    @question.content = ""
+    assert @question.invalid?
+    assert_includes @question.errors.full_messages, "Content can't be blank"
+  end
 end
