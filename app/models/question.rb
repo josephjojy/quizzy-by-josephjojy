9d62ef47a0e97a955555fd01c8c0fff1316a09da
Joseph Jojy <joseph.jojy61@gmail.com>
2021-11-11 19:44:35 +0530

    added Delete cascade for quiz and question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@
 class Question < ApplicationRecord
   belongs_to :quiz
 
-  has_many :options
+  has_many :options, dependent: :destroy
 
   validates :content, presence: true
   validates :quiz_id, presence: true
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,7 +3,7 @@
 class Quiz < ApplicationRecord
   belongs_to :user
 
-  has_many :questions
+  has_many :questions, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :user_id, presence: true
