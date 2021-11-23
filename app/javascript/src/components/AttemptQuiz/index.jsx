97910fe11da031a2a990da75bdd0356fe9c70c3a
Joseph Jojy Chirayath <joseph.chirayath@bigbinary.com>
2021-11-23 21:26:44 +0530

    Fixed issue Invalid slug quiz attempt

--- app/javascript/src/components/AttemptQuiz/index.jsx
+++ app/javascript/src/components/AttemptQuiz/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+import { PageLoader } from "@bigbinary/neetoui/v2";
 import Logger from "js-logger";
 import { useParams } from "react-router-dom";
 
@@ -13,6 +14,7 @@ const AttemptQuiz = () => {
   const [userId, setUserId] = useState();
   const [attemptQuiz, setAttemptQuiz] = useState("");
   const [attemptId, setAttemptId] = useState();
+  const [loading, setLoading] = useState(true);
   attemptId;
 
   const fetchQuizSlug = async () => {
@@ -20,13 +22,23 @@ const AttemptQuiz = () => {
       const response = await quizzesApi.showSlug(slug);
       const { quiz } = await response.data;
       await setAttemptQuiz(quiz);
+      setLoading(false);
     } catch (error) {
       Logger.error(error);
+      window.location.assign(`/public/${slug}`);
     }
   };
   useEffect(() => {
     fetchQuizSlug();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="absolute top-0 left-0 flex flex-col justify-center items-center w-full h-full">
+        <PageLoader />
+      </div>
+    );
+  }
 
   return (
     <>
